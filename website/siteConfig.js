4de7a242c6d4ccb8024288eb7c892586480a96b0
Mike Nelson <miken@vmware.com>
2019-11-01 10:06:32 -0400

    Add first post

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -25,13 +25,13 @@ const siteConfig = {
   subTitle: 'Modern Management Tools & Posts',
   tagline: 'VMware Workspace ONE | Windows 10 | macOS',
   url: 'https://mikenelsonjr.github.io', // Your website URL
-  baseUrl: '/blog/', // Base URL for your project */
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'blog',
+  projectName: 'techblog',
   organizationName: 'mikenelsonjr',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
